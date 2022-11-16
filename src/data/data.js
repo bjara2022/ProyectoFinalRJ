deeb50f0dbd1aa462a43dd7f7c0d259194b4f122
Barbara Jara <jarabarbara.m@gmail.com>
2022-11-16 15:01:34 -0300

    correcciones

--- src/data/data.js
+++ src/data/data.js
@@ -10,6 +10,15 @@ export const data = [
 	},
 	{
 		id: 2,
+		title: 'crema post tattoo',
+		description: 'Crema hidratante curativa',
+		price: 800,
+		category: 'cremas',
+		stock:20,
+		pictureUrl: 'Crema2',
+	},
+	{
+		id: 3,
 		title: 'Jabón Antibacterial',
 		description: 'Jabón liquido',
 		price: 900,
@@ -18,7 +27,16 @@ export const data = [
 		pictureUrl: 'jabon',
 	},
 	{
-		id: 3,
+		id: 4,
+		title: 'Jabón Antibacterial',
+		description: 'Jabón liquido',
+		price: 900,
+		category: 'jabón Limpiador',
+		stock: 5,
+		pictureUrl: 'jabon2',
+	},
+	{
+		id: 5,
 		title: 'Jabón Limpiador',
 		description: 'Jabón para el proceso del tatuaje',
 		price: 1500,
@@ -28,13 +46,22 @@ export const data = [
 	},
 	
 	{
-		id: 4,
+		id: 6,
 		title: 'Agujas',
 		description: 'Agujas de primera calidad caja blanca',
 		price: 1800,
 		category: 'agujas',
 		stock: 5,
 		pictureUrl: 'agujas',
+	},
+	{
+		id: 7,
+		title: 'Agujas',
+		description: 'Agujas de primera calidad caja blanca 3Rl',
+		price: 1800,
+		category: 'agujas',
+		stock: 5,
+		pictureUrl: 'aguja2',
 	},
 	
 ];
